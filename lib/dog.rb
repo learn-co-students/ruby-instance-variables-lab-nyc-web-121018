49c3ebd5f0e41757ad3e57585ab95864d1c822e3
Nison Malayev <nmalayev@gmail.com>
2018-12-11 10:47:25 -0500

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,14 @@
+class Dog # setting method
+  def name=(dog_name)
+    @this_dogs_name = dog_name
+  end
+
+  def name # getting method
+    @this_dogs_name
+  end
+end
+
+lassie = Dog.new
+lassie.name = "Lassie"
+
+puts lassie.name
